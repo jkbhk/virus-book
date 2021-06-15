51d1b51d33693a59fdd0824aa5699f7294d02e65
kunka squishy tentacles <marcusheadgotscarm16@gmail.com>
2021-06-15 20:11:51 +0800

    use environment variables

--- src/news.js
+++ src/news.js
@@ -1,7 +1,10 @@
+import { NEWS_API_KEY } from "react-native-dotenv";
+
 const url =
-  "https://newsapi.org/v2/top-headlines?q=covid&country=sg\&us&apiKey=";
+  "https://newsapi.org/v2/top-headlines?q=covid&country=sg&us&apiKey=" +
+  NEWS_API_KEY;
 
 export async function getNews() {
-  let result = await fetch(url).then(response => response.json());
+  let result = await fetch(url).then((response) => response.json());
   return result.articles;
 }
