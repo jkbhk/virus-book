fcdacb804ee431ec03c1426ce77502cb1321669c
kunka squishy tentacles <marcusheadgotscarm16@gmail.com>
2021-06-15 18:13:52 +0800

    add icons and placeholder

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import NewsScreen from './screens/NewsScreen'
-import LearnScreen from './screens/LearnScreen'
-
-
-
-
+import * as React from "react";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome } from "@expo/vector-icons";
+import NewsScreen from "./screens/NewsScreen";
+import LearnScreen from "./screens/LearnScreen";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Learn") {
+              iconName = focused ? "book" : "book";
+            } else if (route.name === "News") {
+              iconName = focused ? "newspaper-o" : "newspaper-o";
+            }
+
+            // You can return any component that you like here!
+            return <FontAwesome name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "limegreen",
+          inactiveTintColor: "gray",
+        }}
+      >
         <Tab.Screen name="Learn" component={LearnScreen} />
         <Tab.Screen name="News" component={NewsScreen} />
       </Tab.Navigator>
--- screens/LearnScreen.js
+++ screens/LearnScreen.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text, Image } from "react-native";
 
 export default function LearnScreen() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Virus info!</Text>
-      </View>
-    );
-  }
-  
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Many knowledge! Much wow!</Text>
+      <Image
+        style={{ width: 100, height: 100, borderRadius: 30 }}
+        source={{
+          uri: "https://i.pinimg.com/originals/8d/ec/f9/8decf9caed777b8d0d698e01270ce308.png",
+        }}
+      />
+    </View>
+  );
+}
