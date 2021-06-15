ece63aa3d27d5f8f2e5cf2f19c6e2e1f4c3d234c
Hanniedew <hoodieray@gmail.com>
2021-06-16 01:37:40 +0800

    New changes made to hygiene tab

--- screens/LearnScreen.js
+++ screens/LearnScreen.js
@@ -1,29 +1,141 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Button, TouchableOpacity } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-function LearnScreen() {
+const Stack = createStackNavigator();
+
+export default function MyStack() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Papaya is life</Text>
-      <Image
-        style={{ width: 100, height: 100, borderRadius: 30 }}
-        source={{
-          uri: "https://i.pinimg.com/originals/8d/ec/f9/8decf9caed777b8d0d698e01270ce308.png",
-        }}
+    <Stack.Navigator>
+      <Stack.Screen name="Learn" component={LearnScreen} />
+      <Stack.Screen
+        name="Hand Washing Techniques"
+        component={HandWashingScreen}
       />
+      <Stack.Screen
+        name="Mask Wearing Methodology"
+        component={MaskWearingScreen}
+      />
+      <Stack.Screen name="More Details" component={DetailsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function LearnScreen({ navigation }) {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "Top Left",
+        justifyContent: "Top",
+        textAlign: "center",
+        padding: 25,
+      }}
+    >
+      <TouchableOpacity
+        style={{ color: "lightblue", padding: 25 }}
+        onPress={() => navigation.navigate("Hand Washing Techniques")}
+      >
+        <Text
+          style={{
+            color: "white",
+            fontSize: 40,
+            backgroundColor: "lightblue",
+          }}
+        >
+          Proper Hand Washing
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{ color: "lightblue", padding: 25 }}
+        onPress={() => navigation.navigate("Mask Wearing Methodology")}
+      >
+        <Text
+          style={{
+            color: "white",
+            fontSize: 40,
+            backgroundColor: "lightblue",
+          }}
+        >
+          Proper Mask Wearing
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const Stack = createStackNavigator();
-
-export default function LearnStack() {
+function HandWashingScreen({ navigation }) {
   return (
-    <Stack.Navigator
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    >
-      <Stack.Screen name="Learn" component={LearnScreen} />
-    </Stack.Navigator>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Image
+        style={{
+          width: 500,
+          height: 500,
+          borderRadius: 0,
+          resizeMode: "contain",
+        }}
+        source={{
+          uri: "https://www.healthhub.sg/sites/assets/Assets/Article%20Images/washing_hands_photos.jpg",
+        }}
+      />
+
+      <TouchableOpacity
+        style={{ color: "lightblue" }}
+        onPress={() => navigation.navigate("More Details")}
+      >
+        <Text
+          style={{
+            color: "white",
+            fontSize: 20,
+            backgroundColor: "lightblue",
+            alignItems: "bottomleft",
+          }}
+        >
+          For More Details Click Here
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function MaskWearingScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Image
+        style={{
+          width: 500,
+          height: 500,
+          borderRadius: 0,
+          resizeMode: "contain",
+        }}
+        source={{
+          uri: "https://www.who.int/images/default-source/health-topics/coronavirus/masks-infographic---final-(web---rgb).png?sfvrsn=c67232f0_19",
+        }}
+      />
+      <TouchableOpacity
+        style={{ color: "lightblue" }}
+        onPress={() => navigation.navigate("More Details")}
+      >
+        <Text
+          style={{
+            color: "white",
+            fontSize: 20,
+            backgroundColor: "lightblue",
+            alignItems: "bottomleft",
+          }}
+        >
+          For More Details Click Here
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+    ></View>
   );
 }
